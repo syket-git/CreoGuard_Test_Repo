8b833119560f6099e36e909fe3b327464c7a692e
Syket Bhattachergee <syketweb@gmail.com>
2025-12-28 12:14:42 +0600

    created new branch

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-password = "secret123"
-query = "SELECT * FROM users WHERE id = " + user_id
+# Bad code for testing CreoGuard
+
+password = "admin123"
+api_key = "sk-secret-key-12345"
+db_password = "root123"
+
+def get_user(user_id):
+    query = "SELECT * FROM users WHERE id = " + user_id
+    return query
+
+def login(username, password):
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    return query
+
+eval(input("Enter command: "))
+
+import pickle
+data = pickle.loads(user_input)
